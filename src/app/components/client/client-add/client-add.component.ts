b32ce6d6602524478eb87723ab79dce9497c95ef
badr0badr <badr.emad99@gmail.com>
2025-07-20 23:31:21 +0300

    v

--- src/app/components/client/client-add/client-add.component.ts
+++ src/app/components/client/client-add/client-add.component.ts
@@ -10,43 +10,46 @@ import { ClientServiceService } from 'src/app/Service/client-service.service';
   styleUrls: ['./client-add.component.css']
 })
 export class ClientAddComponent {
-apierror:string='';
-    isloading: boolean = false;
+  apierror: string = '';
+  isloading: boolean = false;
   addclientform: FormGroup = new FormGroup({
-      id: new FormControl(null, [Validators.pattern(/^[0-9]{14}$/),Validators.required]),
-      name: new FormControl(null, [Validators.maxLength(400), Validators.minLength(3),Validators.required]),
-      phoneNumber: new FormControl(null, [Validators.maxLength(11), Validators.minLength(11),Validators.required]),
-      address: new FormControl(null, [Validators.maxLength(700), Validators.minLength(10),Validators.required])
-    });
-    addclientformdata: ClientEditAddData =undefined!;
-    constructor(private _AuthService: ClientServiceService, private _Router: Router) { 
-    }
-    
-    addclient(dataform: FormGroup) {
-        this.addclientformdata = {
-          name: dataform.value.name,
-          address: dataform.value.address,
-          id: dataform.value.id,
-          phoneNumber: dataform.value.phoneNumber
-        };
-    
-        this.isloading = true;
-        if (dataform.valid) {
-          this._AuthService.ClientEdit(this.addclientformdata).subscribe({
-            next: (response) => {
-              if (response.status === 200) {
-                this._Router.navigate(['/client']);
-                this.isloading = false;
-              } else {
-                this.apierror = "Failed to add Client. Please try again.";
-                this.isloading = false;
-              }
-            },
-            error: (err:any) => {
-              this.isloading = false;
-              this.apierror=err.error.message;  
-            }
-          })
+    id: new FormControl(null, [Validators.pattern(/^[0-9]{14}$/), Validators.required]),
+    name: new FormControl(null, [Validators.maxLength(400), Validators.minLength(3), Validators.required]),
+    phoneNumber: new FormControl(null, [Validators.maxLength(11), Validators.minLength(11), Validators.required]),
+    address: new FormControl(null, [Validators.maxLength(700), Validators.minLength(10), Validators.required])
+  });
+  addclientformdata: ClientEditAddData = undefined!;
+  constructor(private _AuthService: ClientServiceService, private _Router: Router) {
+  }
+
+  addclient(dataform: FormGroup) {
+    this.addclientformdata = {
+      name: dataform.value.name,
+      address: dataform.value.address,
+      id: dataform.value.id,
+      phoneNumber: dataform.value.phoneNumber
+    };
+
+    this.isloading = true;
+    if (dataform.valid) {
+      this._AuthService.ClientEdit(this.addclientformdata).subscribe({
+        next: (response) => {
+          if (response.status === "200") {
+            this._Router.navigate(['/client']);
+            this.isloading = false;
+          } else {
+            this.apierror = response.message;
+            this.isloading = false;
+          }
+        },
+        error: (err: any) => {
+          this.isloading = false;
+          this.apierror = err.error.message;
+          if (err.error.errors) {
+            this.apierror += '\n' + Object.values(err.error.errors).flat().join(', ');
+          }
         }
-      }
+      })
+    }
+  }
 }
